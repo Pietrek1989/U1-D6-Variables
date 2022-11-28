db393208f117bd6b067b545905c13e232f95de6e
Pietrek1989 <68666992+Pietrek1989@users.noreply.github.com>
2022-11-28 15:09:24 +0100

    Variable homework solution

---
+++ index.js
@@ -0,0 +1,11 @@
+let firstName = "Piotr";
+let secondName = "Pawel";
+let lastName = "Rodzen";
+let fullName = firstName + " " + secondName + " " + lastName;
+console.log("\nHello, my name is ", fullName);
+
+let age = 33;
+let year = 2022;
+console.log("\nMy year of birth is ", year - age);
+
+
